e221217e2fb76d23e93b20cb614c4b60bcc00ace
EC2 Default User <ec2-user@ip-172-31-28-19.us-west-1.compute.internal>
2022-06-28 12:36:39 +0000

    スケジュール課題提出

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,6 @@
 class SchedulesController < ApplicationController
   def index
-    @schedules = Schedule.all
+    @schedules = Schedule.all.order(start: "ASC")
   end
   
   def new
@@ -8,8 +8,9 @@ class SchedulesController < ApplicationController
   end
   
   def create
-    @schedule = Schedule.new(params.require(:schedule).parmit(:title,:start,:end,:period))
+    @schedule = Schedule.new(params.require(:schedule).permit(:title,:start,:end,:period,:memo))
     if @schedule.save
+       flash[:notice] = "ユーザーを新規登録しました"
       redirect_to:schedules
     else
       render"new"
@@ -18,14 +19,28 @@ class SchedulesController < ApplicationController
   end
   
   def show
+    @schedule = Schedule.find(params[:id])
   end
   
   def edit
+    @schedule = Schedule.find(params[:id])
   end
   
   def update
+    @schedule = Schedule.find(params[:id])
+    if @schedule.update(params.require(:schedule).permit(:title,:start,:end,:period,:memo))
+      flash[:notice]="スケジュールの情報を更新しました"
+      redirect_to:schedules
+    else
+      render"edit"
+    end
   end
+
   
   def destroy
+    @schedule = Schedule.find(params[:id])
+    @schedule.destroy
+    flash[:notice] = "スケジュールを削除しました"
+    redirect_to:schedules
   end
 end
