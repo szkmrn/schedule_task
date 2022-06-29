52feaad4a2fae9e786606f4f54f1261585a168de
EC2 Default User <ec2-user@ip-172-31-28-19.us-west-1.compute.internal>
2022-06-29 14:57:30 +0000

    バリデーションのif分のカラム名にself.を追記

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,7 +2,7 @@ class Schedule < ApplicationRecord
     validate :start_end_check
     
   def start_end_check
-    if :end < :start 
+    if self.end < self.start 
       errors.add(:end,"は開始日より前の日付を登録できません") 
     end
   end
